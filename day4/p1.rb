99b0f7cfcc89b4db29889493200e845ddbd2585a
Chris W Jones <chris.jones@cbsinteractive.com>
2018-12-04 12:45:37 -0500

    d4p1: sort log lines

--- day4/p1.rb
+++ day4/p1.rb
@@ -1,5 +1,31 @@
 #!/usr/bin/env ruby
+require 'date'
+
+class LogLine
+  include Comparable
+  attr_reader :date, :log
+
+  LOG_REGEX = /\[(?<year>\d+)-(?<month>\d+)-(?<day>\d+) (?<hour>\d+):(?<minute>\d+)\] (?<log>.*)/
+
+  def initialize(line)
+    matches = LOG_REGEX.match(line)
+    @date = DateTime.new(matches[:year].to_i, matches[:month].to_i, matches[:day].to_i, matches[:hour].to_i, matches[:minute].to_i)
+    @log = matches[:log]
+  end
+
+  def pretty_date
+    @date.strftime("%F %R")
+  end
+
+  def to_s
+    "[#{pretty_date}] #{@log}"
+  end
+
+  def <=>(another)
+    @date <=> another.date
+  end
+end
+
 
 input = ARGV[0]
-lines = File.readlines(input)
-
+log_lines = File.readlines(input).map{|line| LogLine.new(line)}.sort
